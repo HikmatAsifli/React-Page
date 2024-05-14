3036e1f3408fec82badd8efe6fb6ffbe39f7a501
Hikmat Guliyev <hikmatasifli@gmail.com>
2024-05-14 10:32:17 +0400

    cards updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,29 @@
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import ROUTES from "./Routes/routes";
 import MainContext from "./context/context";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css'
+import axios from "axios";
 
 function App() {
     const[loading, setLoading] = useState(true);
     const[error, setError] = useState('false');
     const [data, setData] = useState([]);
     const router = createBrowserRouter(ROUTES);
+    useEffect(()=>{
+        axios.get('http://localhost:3000/products').then(res=>{
+            setData([...res.data])
+        }).catch(error =>{
+            setError(error)
+        })
+    }, [])
+    const contextData = {
+        data, setData, loading, setLoading, error, setError
+      }
     return (
         <>
-        <MainContext.Provider value={{data, setData, loading, setLoading, error, setError}}>
+        <MainContext.Provider value={contextData}>
             <RouterProvider router={router }/>
         </MainContext.Provider>
         </>
--- src/components/site/card/Card.jsx
+++ src/components/site/card/Card.jsx
@@ -1,22 +1,28 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import './Card.css'
+import MainContext from '../../../context/context'
 
 const Card = () => {
-  return (
-    <div className="container mt-5">
-        <div className="row">
-            <div className="col-lg-3 col-md-6 col-sm-12">
-                <div className="card">
-                    <img src="https://preview.colorlib.com/theme/shop/img/l5.jpg.webp" className="card-img-top" alt="..."/>
-                    <div className="card-body">
-                        <h5 className="card-title">Card title</h5>
-                        <h4>Price</h4>
-                    </div>
-                </div>
+    const {data} = useContext(MainContext)
+    return (
+        <div className="container mt-5">
+            <div className="row">
+                {
+                    data.map(item => (
+                        <div className="col-lg-3 col-md-6 col-sm-12">
+                            <div className="card">
+                                <img src={item.image} className="card-img-top" alt="..." width="300px" height="300px" />
+                                <div className="card-body">
+                                    <h5 className="card-title">{item.name}</h5>
+                                    <h4>{item.price}</h4>
+                                </div>
+                            </div>
+                        </div>
+                    ))
+                }
             </div>
         </div>
-    </div>
-  )
+    )
 }
 
 export default Card
